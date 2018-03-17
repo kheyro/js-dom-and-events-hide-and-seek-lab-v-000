64f21220608e177fed714e489973ce63508c462d
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 18:36:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  let parent = document.getElementsById("grand-node")
+  let parent = document.getElementById("grand-node")
   return parent.querySelector('div div div div')
 }
