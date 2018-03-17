bcfd8e3b7ba3ae05f05578a6b40a58ca12a02033
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 18:28:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(el) {
 function nestedTarget() {
   return document.getElementsByClassName('target')[0]
 }
+
+function deepestChild() {
+  let parent = document.getElementsById("grand-node").children
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
