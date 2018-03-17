f4eadb97787e13ff88bdc4c7e3fd680ccda64c3d
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 18:42:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(el) {
+  return document.querySelector(el)
+}
+
+function nestedTarget() {
+  return document.getElementsByClassName('target')[0]
+}
+
+function deepestChild() {
+  let parent = document.getElementById("grand-node")
+  return parent.querySelector('div div div div div')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].textContent) + n).toString()
+  }
+}
