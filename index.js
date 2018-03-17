f176c4a5623defc79d36fb2046d7776bf69c1ea0
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 18:38:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function deepestChild() {
   let parent = document.getElementById("grand-node")
   return parent.querySelector('div div div div div')
 }
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + n).toString()
+  }
+}
